e0a6db91176d72a7defa24e7799745c7435a3a70
DeJay <contact@dejaydev.com>
2019-03-11 12:32:10 -0500

    Remove JS requirements

--- js/index.js
+++ js/index.js
@@ -27,41 +27,27 @@ $.fn.extend({
     },
 });
 
-function showDiscord() {
-    var infoDiv = $(document.getElementsByClassName("infoDiv")[0]);
-    infoDiv.animateCss('tada')
-    infoDiv.html('DeJay#1337')
+function showText(text) {
+    var infoDiv = $('.infoDiv');
+    infoDiv.animateCss('tada');
+    infoDiv.text(text);
 }
 
-function showTwitter() {
-    var infoDiv = $(document.getElementsByClassName("infoDiv")[0]);
-    infoDiv.animateCss('tada')
-    infoDiv.html('<a href="https://twitter.com/DeJayDev">@DeJayDev</a>')
-}
-
-function showBlog() {
-    var infoDiv = $(document.getElementsByClassName("infoDiv")[0]);
-    infoDiv.animateCss('rubberBand')
-    infoDiv.html('<a href="https://blog.dejaydev.com">Blog!</a>')
-}
-
-function showGit() {
-    var infoDiv = $(document.getElementsByClassName("infoDiv")[0]);
-    infoDiv.animateCss('tada')
-    infoDiv.html('<a href="https://github.com/DeJayDev">DeJayDev</a>')
-}
-
-function showEmail() {
-    var infoDiv = $(document.getElementsByClassName("infoDiv")[0]);
-    infoDiv.animateCss('tada')
-    infoDiv.html('<a href="mailto:contact@dejaydev.com">contact@dejaydev.com</a>')
+function clearText() {
+    var infoDiv = $('.infoDiv');
+    infoDiv.text('');
 }
 
 $(function () {
     var origin = location.hostname.toLowerCase();
     var warning = $(document.getElementsByClassName("warning")[0]);
+
     if (origin.includes("dejay") || origin.includes("127.0.0.1")) return;
-    
+
     warning.html('It appears you\'ve stumbled upon: ' + origin + '!' +
-    '<br> This page is a property owned by DeJay, and will redirect when it is prepared.')
+        '<br> This page is a property owned by DeJay, and will redirect when it is prepared.')
+
+    showText = function (text) {
+
+    }
 });
