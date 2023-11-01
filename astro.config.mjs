7d3244981109513c3d072b021363a5a551a747e3
Dj Isaac <contact@dejaydev.com>
2023-11-01 14:52:13 -0500

    astroify

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+
+import svelte from "@astrojs/svelte";
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [svelte()],
+  output: "server",
+  adapter: cloudflare()
+});
