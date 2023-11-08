466fadd5844cd0c96531816e3e1dc74415f5af0b
Dj Isaac <contact@dejaydev.com>
2023-11-08 12:54:19 -0600

    wtf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 import svelte from "@astrojs/svelte";
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
