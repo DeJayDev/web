33ba13076af44642db3bda45c492e4355f0709bf
Dj Isaac <contact@dejaydev.com>
2023-11-22 16:49:36 -0600

    ditch svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
-
-import svelte from "@astrojs/svelte";
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [preact()],
   output: "static"
 });
