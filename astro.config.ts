de5db74e833d8974b4f6d051156de84336f798e3
Dj Isaac <contact@dejaydev.com>
2023-12-16 03:25:25 -0600

    huge overhaul
    fixes mobile issues
    break everything into more aggressive components
    copy seo from original website
    disable darkreader by force.

--- astro.config.ts
+++ astro.config.ts
@@ -4,5 +4,8 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
-  output: "static"
+  output: "static",
+  server: {
+    port: 3000
+  }
 });
