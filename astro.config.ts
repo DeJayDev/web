db44723dbab4f72b6df4d91b97a98f8c1ca5baab
Dj Isaac <contact@dejaydev.com>
2024-03-01 18:08:35 -0600

    redirects!
    no need to keep the pdf in the repo.

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
@@ -7,5 +7,5 @@ export default defineConfig({
   output: "static",
   server: {
     port: 3000
-  }
+  },
 });
