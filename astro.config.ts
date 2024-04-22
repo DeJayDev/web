444723dca2a369904021a09326d48cba3ad98a0d
Dj Isaac <contact@dejaydev.com>
2024-04-22 14:38:46 -0500

    biome! :D

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,12 @@
-import preact from "@astrojs/preact";
-import icon from "astro-icon";
-import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact"
+import icon from "astro-icon"
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), preact()],
-  output: "static",
-  server: {
-    port: 3000
-  },
-});
+    integrations: [icon(), preact()],
+    output: "static",
+    server: {
+        port: 3000,
+    },
+})
