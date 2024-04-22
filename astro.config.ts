d336ae9da9c773ef1864df4d15338b087a443295
Dj Isaac <contact@dejaydev.com>
2024-04-22 14:24:06 -0500

    somehow icons were broken on prod and noone told me

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,10 @@
 import preact from "@astrojs/preact";
+import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
+  integrations: [icon(), preact()],
   output: "static",
   server: {
     port: 3000
