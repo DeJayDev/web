2ccaf158a22b5dd259c0d001fd92c6482a8eef60
Dj Isaac <contact@dejaydev.com>
2024-10-25 13:55:07 -0500

    :arrow_up: dep push

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,32 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
 
-import cloudflare from "@astrojs/cloudflare";
-import icon from "astro-icon";
+import cloudflare from "@astrojs/cloudflare"
+import icon from "astro-icon"
 
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare({
-    imageService: "noop"
+    imageService: "passthrough",
   }),
   experimental: {
-    serverIslands: true
+    serverIslands: true,
   },
-  integrations: [icon({
-    include: {
-      "simple-icons": ["x", "discord"],
-      heroicons: ["envelope-open-20-solid"]
-    }
-  })],
+  integrations: [
+    icon({
+      include: {
+        "simple-icons": ["x", "discord"],
+        heroicons: ["envelope-open-20-solid"],
+      },
+    }),
+  ],
   output: "hybrid",
   server: {
-    port: 3000
+    port: 3000,
   },
   site: "https://dejay.dev",
   vite: {
     css: {
-      transformer: "lightningcss"
-    }
+      transformer: "lightningcss",
+    },
   },
-});
+})
