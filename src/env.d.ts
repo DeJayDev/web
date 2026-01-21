26eb79d8603b89b6d0cfbc6992162944727c3fcb
Dj Isaac <contact@dejaydev.com>
2026-01-20 19:10:33 -0800

    chore: formatting...

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
-type Runtime = import('@astrojs/cloudflare').Runtime<Env>;
+type Runtime = import("@astrojs/cloudflare").Runtime<Env>
 
 declare namespace App {
-  interface Locals extends Runtime {}
+    interface Locals extends Runtime {}
 }
